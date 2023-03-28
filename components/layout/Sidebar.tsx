5d7142e11069a8e4b22bacbf6f83a0296cca7b1e
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-28 12:50:49 +0630

    some code in sidebar

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,5 +1,26 @@
+import { BsHouseFill, BsBellFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
+
 const Sidebar = () => {
-    return ( <div></div> );
-}
- 
+  const items = [
+    {
+      icon: BsHouseFill,
+      label: "Home",
+      href: "/",
+    },
+    {
+      icon: BsBellFill,
+      label: "Notifications",
+      href: "/notifications",
+    },
+    {
+      icon: FaUser,
+      label: "Profile",
+      href: `/users/123`,
+    },
+  ];
+
+  return <div></div>;
+};
+
 export default Sidebar;
