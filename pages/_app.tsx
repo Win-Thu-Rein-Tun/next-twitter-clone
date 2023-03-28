c8ec7ae5cf73be65b6b7b4c8e4ac41750b9e86d3
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-28 22:19:28 +0630

    create loginmodal in helpers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
-import Modal from "@/components/Modal";
+import LoginModal from "@/components/helpers/LoginModal";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <LoginModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
