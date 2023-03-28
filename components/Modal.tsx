6a697ff203ecac5ec1c138f2030fc4d82225b1a4
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-28 16:11:15 +0630

    add modal

---
+++ components/Modal.tsx
@@ -0,0 +1,47 @@
+import { useCallback } from "react";
+
+interface ModalProps {
+  isOpen?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  title?: string;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
+  actionLabel: string;
+  disabled?: boolean;
+}
+
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  onClose,
+  onSubmit,
+  title,
+  body,
+  actionLabel,
+  footer,
+  disabled,
+}) => {
+  const handleClose = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+
+    onClose();
+  }, [onClose, disabled]);
+
+  const handleSubmit = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+
+    onSubmit();
+  }, [onSubmit, disabled]);
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return <></>;
+};
+
+export default Modal;
