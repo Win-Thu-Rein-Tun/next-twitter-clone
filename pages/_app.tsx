d074568222453f75d824932f5468bff5f795d759
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-31 15:11:03 +0630

    code editmodal.tsx in helpers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,13 @@ import LoginModal from "@/components/helpers/LoginModal";
 import RegisterModal from "@/components/helpers/RegisterModal";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
+import EditModal from "@/components/helpers/EditModal";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Toaster />
+      <EditModal />
       <RegisterModal />
       <LoginModal />
       <Layout>
