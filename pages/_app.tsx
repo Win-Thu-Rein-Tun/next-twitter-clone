32a9d5e9e2a42a0075824bd1221e83e6d8abd636
Sun-D-Shine <winthureintun2000@gmail.com>
2023-04-03 17:54:33 +0630

    change helpers name to modals

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
-import LoginModal from "@/components/helpers/LoginModal";
-import RegisterModal from "@/components/helpers/RegisterModal";
+import LoginModal from "@/components/modals/LoginModal";
+import RegisterModal from "@/components/modals/RegisterModal";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
-import EditModal from "@/components/helpers/EditModal";
+import EditModal from "@/components/modals/EditModal";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
