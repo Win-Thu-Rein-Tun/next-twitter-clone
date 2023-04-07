e058302a86b09ef2dec56b9c028ed4be3e54e63b
Sun-D-Shine <winthureintun2000@gmail.com>
2023-04-07 20:51:46 +0630

    commit

--- pages/api/comments.ts
+++ pages/api/comments.ts
@@ -33,7 +33,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     //     }
     //   }); 
 
-    //   if (post?.userId) {
+    //   if (post?.userId) { 
     //     await prisma.notification.create({
     //       data: {
     //         body: 'Someone replied on your tweet!',
